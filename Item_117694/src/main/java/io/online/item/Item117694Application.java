73b320d0564f0b597c965c7c41b38205edf2a0d5
poojaa-v <pooja.venkataraman@cognizant.com>
2018-11-28 13:16:43 +0530

    Base Version

---
+++ Item_117694/src/main/java/io/online/item/Item117694Application.java
@@ -0,0 +1,12 @@
+package io.online.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Item117694Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Item117694Application.class, args);
+	}
+}
